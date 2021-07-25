bd4967fc80584fd546b5812931657d34ecb9712d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-25 20:31:32 +0200

    Remove unused dependencies

--- modules/app-shell/snowpack.config.js
+++ modules/app-shell/snowpack.config.js
@@ -6,40 +6,14 @@ module.exports = {
     public: { url: '/', static: true },
     src: { url: '/dist' },
   },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-  ],
-  routes: [
-    /* ... */
-  ],
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  routes: [],
   optimize: {
     bundle: true,
     sourcemap: false,
   },
   packageOptions: {
     NODE_ENV: true,
-    rollup: {
-      plugins: [postcss({
-        extract: false,
-        minimize: true,
-        loaders: [lessLoader({
-          nodeModulePath: "./node_modules",
-          aliases: {}
-      })],
-        use: [
-          [
-            "less",
-            {
-              includePaths: [
-                "./src",
-                "./node_modules"
-              ],
-            },
-          ],
-        ],
-      })],
-    }
   },
   devOptions: {
     open: 'none',
