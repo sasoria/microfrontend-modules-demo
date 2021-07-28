96e2d906c79fc1b7098339f5f85f4223778aafd0
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-07-28 21:21:07 +0200

    Add lazy loading of apps

--- app-c/src/App.jsx
+++ app-c/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'https://cdn.skypack.dev/react';
-import AppA from 'http://localhost:7100/build/dist/index.js';
-import AppB from 'http://localhost:7200/build/dist/index.js';
+
+const AppA = React.lazy(() =>
+  import('http://localhost:7100/build/dist/index.js'),
+);
+
+const AppB = React.lazy(() =>
+  import('http://localhost:7200/build/dist/index.js'),
+);
 
 function App() {
   return (
     <section>
-      <AppA />
-      <AppB />
+      <React.Suspense fallback="Loading...">
+        <AppA />
+        <AppB />
+      </React.Suspense>
     </section>
   );
 }
